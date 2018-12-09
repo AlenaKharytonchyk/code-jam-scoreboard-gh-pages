52ad5395d8f01819a6449ccfef80b9347f49c84d
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2018-12-09 21:07:56 +0300

    feat: add compiled sources

---
+++ precache-manifest.8d295af056d0ea4b6d57a82906d27579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a824d7e120d1d076a52f",
+    "url": "/static/js/main.a824d7e1.chunk.js"
+  },
+  {
+    "revision": "f00fb236e2b87dd5c7a2",
+    "url": "/static/js/1.f00fb236.chunk.js"
+  },
+  {
+    "revision": "a824d7e120d1d076a52f",
+    "url": "/static/css/main.19a3ec48.chunk.css"
+  },
+  {
+    "revision": "4b6e224bb34a2acf4fabd6a02c1bc195",
+    "url": "/index.html"
+  }
+];
